665750d78bda01c3b53c483b335eba4e11f55a68
Sanek_KZ <alexandervarnaev@gmail.com>
2018-09-18 19:21:50 +0300

    work is done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from './modules/sum';
+import guard from './modules/guard';
+
+console.log (sum(1,25));
+console.log (guard ('Dima'));
---
+++ src/modules/guard.js
@@ -0,0 +1,7 @@
+export default function (name) {
+    if ( name == 'Dima') {
+        return 'Hello, Sasha! Come in, please!'
+    }else{
+        return 'Get out!';
+    }
+};
